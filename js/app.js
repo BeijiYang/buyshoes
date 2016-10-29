5d9357d04407034f2c02596719a4ad6a9b47a34f
BeijiYang <langyuyang@outlook.com>
2016-10-29 18:20:43 +0800

    layout

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+function makeCartScrollNicely() {
+  var cart = document.querySelector(".cart__content");
+  Ps.initialize(cart);
+}
+
 window.onload = function() {
   console.log("page loaded");
+  makeCartScrollNicely();
 }
